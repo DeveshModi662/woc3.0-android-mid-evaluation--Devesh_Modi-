816f5ff36ed49128e45a259439522839e6a46290
Devesh <201901173@daiict.ac.in>
2021-01-16 16:18:07 +0530

    Completed basic main_activity for monday

--- app/src/main/java/com/example/woc30_android_mid_evaluation_devesh_modi/MainActivity.java
+++ app/src/main/java/com/example/woc30_android_mid_evaluation_devesh_modi/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.woc30_android_mid_evaluation_devesh_modi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        ListView myIntro = findViewById(R.id.myListView) ;
+//        ArrayList<String> intro = new ArrayList<>() ;
+//        intro.add("Student's name   : Devesh Modi") ;
+//        intro.add("Student's course : Btech ICT 2019") ;
+//        intro.add("Student's email  : 201901173@daiict.ac.in") ;
+    }
+
+    public void showMonday(View view) {
+        Intent intent = new Intent(this, myMonday.class) ;
+        startActivity(intent) ;
     }
 }
